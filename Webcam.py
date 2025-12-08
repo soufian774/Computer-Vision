39d27685f57758ce6c13ab63e3638cd23ea9ce13
soufian774 <soufianmarkouni3@gmail.com>
2025-12-08 16:46:34 +0100

    Detection Smile

--- Webcam.py
+++ Webcam.py
@@ -12,6 +12,9 @@ cap = cv2.VideoCapture(0)   #Passo 0 per collegarmi alla WebCam principale
 codec = cv2.VideoWriter_fourcc(*"MJPG")
 out = cv2.VideoWriter('output.avi', codec, 30, (640,480))
 
+face_cascade = cv2.CascadeClassifier("face_detection/haarcascade_frontalface_default.xml")
+smile_casdade = cv2.CascadeClassifier("face_detection/haarcascade_smile.xml")
+
 #ret, frame = cap.read()     #ret contiene True o False, cioè se la camera funziona o meno
 
 if(not(cap.isOpened())):
@@ -26,6 +29,8 @@ if(not(cap.isOpened())):
 
 while(cap.isOpened()):
     ret, frame = cap.read()     #ret contiene True o False, cioè se la camera funziona o meno
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     if(bg_mode):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -42,10 +47,18 @@ while(cap.isOpened()):
         cv2.circle(frame, (frame.shape[1] - 20, 20), 8, (0,0,255), cv2.FILLED)
         out.write(frame)
     
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     
+    for (xf,yf,wf,hf) in faces:
+        cv2.rectangle(frame, (xf,yf), (xf+wf, yf+hf), (255,0,0), 3)
+        cropped = frame[yf:(yf+hf),xf:(xf+wf)]
+        smiles = smile_casdade.detectMultiScale(cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY), 1.5, 20)
+        for (xs,ys,ws,hs) in smiles:
+            cv2.rectangle(frame, (xf+xs,yf+ys), (xf+xs+ws, yf+ys+hs), (255,0,0), 3)
+            cv2.imwrite("smile.jpg", cropped)
+        
     cv2.imshow("Webcam", frame)
     k = cv2.waitKey(1)
-    
     
     if(k==ord("b")):
         bg_mode = not(bg_mode)
